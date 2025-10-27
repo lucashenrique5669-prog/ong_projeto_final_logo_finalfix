ab7870e027fe6ce842d8ad7af8d846cf9e07c04d
lucashenrique5669-prog <lucashenrique5669@gmail.com>
2025-10-27 18:59:27 -0300

    Add hamburger menu toggle functionality

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,1 +1,9 @@
+// MENU HAMBURGUER
+const hamburguer = document.querySelector(".hamburguer");
+const navLinks = document.querySelector(".nav-links");
+
+hamburguer.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburguer.classList.toggle("active");
+});
 
